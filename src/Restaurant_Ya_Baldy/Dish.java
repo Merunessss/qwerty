c9e136f91b2f760e219e58927a4cb62b0afdd707
Merunes <merunessss@gmail.com>
2019-12-28 19:11:32 +0200

    base1

--- src/Restaurant_Ya_Baldy/Dish.java
+++ src/Restaurant_Ya_Baldy/Dish.java
@@ -1,14 +1,27 @@
 package Restaurant_Ya_Baldy;
 
-public enum  Dish {
-
-    Bors(150), Fandu(230), Sushi(180);
-
-    Dish(int price) {
+public class  Dish implements  Comparable<Dish> {
+    int price;
+    int id;
+    String name;
+    Dish(int price,String name, Restaurant rest) {
         this.price = price;
+        this.name = name;
+        rest.eDish.add(this);
     }
 
-    int price;
 
+    @Override
+    public int compareTo(Dish o) {
+        return this.name.compareTo(o.name);
+    }
 
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "price=" + price +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
